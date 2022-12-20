5169e9fb5fecad840c12131d649d3b0a52e1269e
ESPORTS-141\cre <t-shirai@oca.ac.jp>
2022-12-20 19:13:16 +0900

    bug4 修正

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -14,16 +14,44 @@
 <body class="">
     <x-header></x-header>
     <div class="page post-page">
-        <form class="form" action="/post" method="post">
+        <form class="form" name = "postContent" action="/post" method="post">
             @csrf
-            <textarea name="postContent" id="" cols="30" rows="5" placeholder="いまどうしてる?"></textarea>
+            <textarea name="postContent" id="postContent" cols="30" rows="5" placeholder="いまどうしてる?"></textarea>
+             @error('postContent')
+                                    {{ $message }}
+                            @enderror
             <div class="post-button">
-                <button class="button-white" type="submit">投稿する</button>
+                <button class="button-white" id="postbtn" type = button>投稿する</button>
             </div>
         </form>
     </div>
 </body>
+
 <script src="{{ asset('/js/app.js') }}"></script>
+
+<!-- Bug 4(JS) -->
+<script>
+    window.onload = function(){
+    const postbtnSubmit =  document.getElementById('postbtn');
+    const inputPost = document.getElementById('postContent');
+
+    postbtnSubmit.addEventListener('click',  function(event){
+        let message = [];
+        
+        if(inputPost.value==""){
+            message.push("投稿内容が未入力です。");
+        }else if(inputPost.value.length > 140){
+            message.push("入力可能文字数は140文字以内です。")
+        }
+        if(message.length > 0){
+            alert(message);
+            return;
+        }
+        document.postContent.submit();
+    });
+}
+</script> 
+
 <style scoped>
     .post-page .form {
         display: flex;
